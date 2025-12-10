bb72dd6b91c6d5c0767fc15ab09dd68cc3251fa3
smthemex <138738845+smthemex@users.noreply.github.com>
2025-12-10 19:59:54 +0800

    Update util.py

--- src/flux/util.py
+++ src/flux/util.py
@@ -235,11 +235,11 @@ def load_from_repo_id(repo_id, checkpoint_name):
     sd = load_sft(ckpt_path, device='cpu')
     return sd
 
-def load_flow_model(name,ckpt_path,cf_model,user_accelerate: bool = True,use_quantize: bool = False,device: str='cpu'):
+def load_flow_model(name,ckpt_path,cf_model,use_accelerate: bool = True,use_quantize: bool = False,device: str='cpu'):
     # Loading Flux
     print("Init model")
     #ckpt_path = configs[name].ckpt_path
-    if user_accelerate:
+    if use_accelerate:
         from contextlib import nullcontext
         try:
             from accelerate import init_empty_weights,load_checkpoint_and_dispatch
@@ -270,12 +270,10 @@ def load_flow_model(name,ckpt_path,cf_model,user_accelerate: bool = True,use_qua
             del original_sd
             gc.collect()
     else:
-
         model = Flux(configs[name].params).to(torch.bfloat16)
         if use_quantize:
             json_path = os.path.join(folder_paths.base_path, "custom_nodes/ComfyUI_LucidFlux/config.json") #config is for pass block
             # Import here to avoid heavy quanto/transformers side effects at module import time
-        
             if ckpt_path is not None:
                 sd = load_sft(ckpt_path, device='cpu')
             else:
@@ -286,9 +284,7 @@ def load_flow_model(name,ckpt_path,cf_model,user_accelerate: bool = True,use_qua
             print("Start a quantization process...")
             from optimum.quanto import requantize
             requantize(model, sd, quantization_map, device=device)
-            print("Model is quantized!")
-            del sd
-            gc.collect()
+            print("Model is quantized!")  
         else:
             if ckpt_path is not None:
                 sd = load_sft(ckpt_path, device='cpu')
@@ -297,9 +293,11 @@ def load_flow_model(name,ckpt_path,cf_model,user_accelerate: bool = True,use_qua
                 del cf_model
             missing, unexpected = model.load_state_dict(sd, strict=False, assign=True)
             print_load_warning(missing, unexpected)
-            del sd
-            gc.collect()
+        del sd
+        gc.collect()
     return model
+
+
 
 def load_flow_model2(name: str, device: str, hf_download: bool = True):
     # Loading Flux
